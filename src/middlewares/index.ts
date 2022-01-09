cdbe3a7e7419a20e9dbd68a37eb93ac014f54b29
Jack Do <dominhtrijack@gmail.com>
2022-01-09 18:05:32 +0700

    remove yarn-error.log

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,16 +1,18 @@
-import express, { Application } from "express"
-import cors from "cors"
-import path from "path"
-import methodOverride from "method-override"
+import express, { Application } from "express";
+import cors from "cors";
+import path from "path";
+import methodOverride from "method-override";
+import morgan from "morgan";
 
 const middlewares: (app: Application) => void = (app) => {
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(cors({ origin: process.env.CORS_ORIGIN }))
-    app.use(express.static(path.join(__dirname, "../public")))
-    app.use(methodOverride("_method"))
-    app.set("views", path.join(__dirname, "../views"))
-    app.set("view engine", "ejs")
-}
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors({ origin: process.env.CORS_ORIGIN }));
+  app.use(morgan("tiny"));
+  app.use(express.static(path.join(__dirname, "../public")));
+  app.use(methodOverride("_method"));
+  app.set("views", path.join(__dirname, "../views"));
+  app.set("view engine", "ejs");
+};
 
-export default middlewares
+export default middlewares;
